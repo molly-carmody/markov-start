a45f74af565feb24a50a5754278e47021521abb4
mollycarmody <mollycarmody@10.188.68.133>
2017-02-15 16:20:05 -0500

    fixed repeats error

--- src/EfficientMarkov.java
+++ src/EfficientMarkov.java
@@ -29,18 +29,19 @@ public class EfficientMarkov implements MarkovInterface<String>{
 
 		//take last two letters and teh 3 gram seed and thn add the letter that follws the 3gram seed
 		//if 3 gram is last aka value = null(on efolllowing), we end the text with that 3 gram --- stop generating and end
-for(int k =0;k<=myText.length()-myOrder;k++){
-	String TextKey; //creates temporary part of text that going to add
-	TextKey = myText.substring(k,k+myOrder); //sets temp text to a gram
+for(int k =0;k<myText.length()-myOrder;k++){
+	 //creates temporary part of text that going to add
+	String TextKey = myText.substring(k,k+myOrder); //sets temp text to a gram
 	
-	if(!(MarkMap.containsKey(myText))){ //initializes 
-	MarkMap.put(TextKey, new ArrayList<String>());
-	}
+	
 		if(k+myOrder+1>=myText.length()){ //once intialized or if doesn't need to be, checks if its at the end of the text or not
 		CharFollow = PSEUDO_EOS;		//if at the end, the following character is PSEUDO_EOS
 		}
 		else{
 		CharFollow = text.substring(k+myOrder,k+myOrder+1); //if not at the end, the follow character is the following character
+		}
+		if(!(MarkMap.containsKey(TextKey))){ //initializes 
+			MarkMap.put(TextKey, new ArrayList<String>());
 		}
 	MarkMap.get(TextKey).add(CharFollow); //once spot created or not, char follow named, it can now add the folllowin character to the value spot for that key
 }
