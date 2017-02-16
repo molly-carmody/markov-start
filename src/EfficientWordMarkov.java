6347fd02ae8216f8eafee75101bafdf1cb1aa51f
mollycarmody <mollycarmody@10.188.68.133>
2017-02-16 00:39:44 -0500

    errors

--- src/EfficientWordMarkov.java
+++ src/EfficientWordMarkov.java
@@ -63,7 +63,7 @@ public class EfficientWordMarkov implements MarkovInterface<WordGram> {
 
 		WordGram current = new WordGram(myTextArray, index, myOrder);
 		//System.out.printf("first random %d for '%s'\n",index,current);
-		sb.append(current.toString());
+		sb.append(current);
 
 		for(int k=0; k <length-myOrder; k++){
 			ArrayList<String> follows = getFollows(current);
@@ -77,7 +77,8 @@ public class EfficientWordMarkov implements MarkovInterface<WordGram> {
 				//System.out.println("PSEUDO");
 				break;
 			}
-			sb.append(" " + nextItem);
+			sb.append(" ");
+			sb.append(nextItem);
 			current = current.shiftAdd(nextItem);
 		}
 		return sb.toString();
